fd491107ce28d78ef005de3e6084d4e2468343c4
TheJoeCoder <joe@radialbog9.uk>
2022-03-19 15:28:24 +0000

    Modify README and add dependency check

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PRootify
-Easy scripts to run Linux distros in PRoot
+Easy scripts to run Linux distros in PRoot. It's like a virtual machine, but not needing a hypervisor or any root access!
+
+## Usage
+Download and run the script:
+```
+wget -qO - https://raw.githubusercontent.com/Radialbog9/PRootify/main/prootify.sh | bash
+```
+From there, you can install and remove distros, and automatically add optional features in 1 click.
+
+## How it Works
+> PRoot is a user-space implementation of `chroot`, `mount --bind`, and `binfmt_misc`. This means users don't need any privileges or setup to do things like using an arbitrary directory as the new root filesystem...
+
+We use PRoot to be able to run any Linux distro on any Linux system without reinstalls or virtualization.
+
+## Credits
+[AnLinux](https://github.com/EXALAB/AnLinux-App/) as the starting point for this project, so thanks to them!
--- prootify.sh
+++ prootify.sh
@@ -1,17 +1,47 @@
+# ############################## #
+# PRootify                       #
+# by Radialbog9                  #
+# License: GNU GPL3              #
+# File: prootify.sh version 1.0  #
+# ############################## #
+
+function exit_if_cancelled () {
+	if [ $? == 1 ]
+	then
+		echo "Quit."
+		exit 1
+	fi
+}
+
+function check_dependencies () {
+	for var in "$@"; do
+		command -v $var >/dev/null 2>&1 || { echo >&2 "I require $var but it isn't installed! Aborting!"; exit 1; }
+	done
+}
+
+# Check dependencies
+check_dependencies dpkg wget proot
+
+is_termux=no
+if [ pwd == "/data/data/com.termux/files/usr/bin/termux-fix-shebang" ]
+then
+	is_termux=yes
+fi
+
+echo $is_termux
+
+# Get action
 action=$(whiptail --backtitle PRootify \
-	--menu "Select Step" 20 25 25 \
+	--menu "Select Action" 15 40 5 \
 	install "Install Distro" \
 	modify "Add/Remove Features" \
 	uninstall "Remove Distro" \
 	3>&1 1>&2 2>&3)
-if [ $? == 1 ]
-then
-	echo "Cancelled"
-	exit
-fi
+exit_if_cancelled
 
+# Get Distro
 distro=$(whiptail --backtitle PRootify \
-	--menu "Select Distro" 20 25 25 \
+	--menu "Select Distro" 15 40 7 \
 	alpine "Alpine" \
 	arch "Arch Linux" \
 	backbox "BackBox" \
@@ -25,10 +55,12 @@ distro=$(whiptail --backtitle PRootify \
 	void "Void" \
 	opensuse "openSUSE" \
 	3>&1 1>&2 2>&3)
-if [ $? == 1 ]
-then
-	echo "Cancelled"
-	exit
-fi
+exit_if_cancelled
 
-echo Selected Distro: $distro
+# Steps
+case $action in
+	install)
+		# Get architecture
+		case `dpkg --print-architecture` in
+		esac
+esac
