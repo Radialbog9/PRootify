7ce4910f930f063f9c5a63a16ed09138c588086c
TheJoeCoder <joe@radialbog9.uk>
2022-03-19 18:16:37 +0000

    Start work on bootstrap scripts

---
+++ bootstrap-images.sh
@@ -0,0 +1,13 @@
+# ############################## #
+# PRootify                       #
+# by Radialbog9                  #
+# License: GNU GPL3              #
+# File: bootstrap-images.sh      #
+# ############################## #
+
+mkdir -p .tmp/scripts/
+mkdir -p .tmp/backbox/{arm64,armhf,amd64,i386}
+mkdir -p .tmp/debian/{arm64,armhf,amd64,i386}
+mkdir -p .tmp/kali/{arm64,armhf,amd64,i386}
+mkdir -p .tmp/parrot/{arm64,armhf,amd64,i386}
+mkdir -p .tmp/ubuntu/{arm64,armhf,amd64,i386}
