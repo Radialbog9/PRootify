40eb5096a6cf7df623aef32f27c41b40b81807ee
TheJoeCoder <joe@radialbog9.uk>
2022-03-19 18:28:30 +0000

    Finished Bootstrap Images Script

--- bootstrap-images.sh
+++ bootstrap-images.sh
@@ -5,9 +5,40 @@
 # File: bootstrap-images.sh      #
 # ############################## #
 
-mkdir -p .tmp/scripts/
-mkdir -p .tmp/backbox/{arm64,armhf,amd64,i386}
-mkdir -p .tmp/debian/{arm64,armhf,amd64,i386}
-mkdir -p .tmp/kali/{arm64,armhf,amd64,i386}
-mkdir -p .tmp/parrot/{arm64,armhf,amd64,i386}
-mkdir -p .tmp/ubuntu/{arm64,armhf,amd64,i386}
+mkdir -p $PWD/rootfs/
+
+mkdir -p $PWD/.tmp/scripts/
+wget -O $PWD/.tmp/scripts/backbox.sh https://raw.githubusercontent.com/Radialbog9/PRootify/main/bootstrap-scripts/backbox.sh
+wget -O $PWD/.tmp/scripts/debian.sh https://raw.githubusercontent.com/Radialbog9/PRootify/main/bootstrap-scripts/debian.sh
+wget -O $PWD/.tmp/scripts/kali.sh https://raw.githubusercontent.com/Radialbog9/PRootify/main/bootstrap-scripts/kali.sh
+wget -O $PWD/.tmp/scripts/parrot.sh https://raw.githubusercontent.com/Radialbog9/PRootify/main/bootstrap-scripts/parrot.sh
+wget -O $PWD/.tmp/scripts/ubuntu.sh https://raw.githubusercontent.com/Radialbog9/PRootify/main/bootstrap-scripts/ubuntu.sh
+
+mkdir -p $PWD/.tmp/{backbox,debian,kali,parrot,ubuntu}
+
+bash $PWD/.tmp/scripts/backbox.sh arm64 $PWD/.tmp/backbox/arm64
+bash $PWD/.tmp/scripts/backbox.sh armhf $PWD/.tmp/backbox/armhf
+bash $PWD/.tmp/scripts/backbox.sh amd64 $PWD/.tmp/backbox/amd64
+bash $PWD/.tmp/scripts/backbox.sh i386 $PWD/.tmp/backbox/i386
+
+bash $PWD/.tmp/scripts/debian.sh arm64 $PWD/.tmp/debian/arm64
+bash $PWD/.tmp/scripts/debian.sh armhf $PWD/.tmp/debian/armhf
+bash $PWD/.tmp/scripts/debian.sh amd64 $PWD/.tmp/debian/amd64
+bash $PWD/.tmp/scripts/debian.sh i386 $PWD/.tmp/debian/i386
+
+bash $PWD/.tmp/scripts/kali.sh arm64 $PWD/.tmp/kali/arm64
+bash $PWD/.tmp/scripts/kali.sh armhf $PWD/.tmp/kali/armhf
+bash $PWD/.tmp/scripts/kali.sh amd64 $PWD/.tmp/kali/amd64
+bash $PWD/.tmp/scripts/kali.sh i386 $PWD/.tmp/kali/i386
+
+bash $PWD/.tmp/scripts/parrot.sh arm64 $PWD/.tmp/parrot/arm64
+bash $PWD/.tmp/scripts/parrot.sh armhf $PWD/.tmp/parrot/armhf
+bash $PWD/.tmp/scripts/parrot.sh amd64 $PWD/.tmp/parrot/amd64
+bash $PWD/.tmp/scripts/parrot.sh i386 $PWD/.tmp/parrot/i386
+
+bash $PWD/.tmp/scripts/ubuntu.sh arm64 $PWD/.tmp/ubuntu/arm64
+bash $PWD/.tmp/scripts/ubuntu.sh armhf $PWD/.tmp/ubuntu/armhf
+bash $PWD/.tmp/scripts/ubuntu.sh amd64 $PWD/.tmp/ubuntu/amd64
+bash $PWD/.tmp/scripts/ubuntu.sh i386 $PWD/.tmp/ubuntu/i386
+
+cp $PWD/.tmp/{backbox,debian,kali,parrot,ubuntu}/*.xz $PWD/rootfs/
