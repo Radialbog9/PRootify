75540f576dc64e67614c11925481dbaa0f944b01
TheJoeCoder <joe@radialbog9.uk>
2022-03-20 11:04:53 +0000

    Update prootify.sh

--- prootify.sh
+++ prootify.sh
@@ -52,7 +52,7 @@ case "$arch_reported" in
 	i*86 | x86)
 		arch="i386" ;;
 	*)
-		whiptail --backtitle PRootify --msgbox "Error: Couldn't determine your architecture. Cannot continue." 15 40
+		whiptail --backtitle PRootify --msgbox "Error: Couldn't determine your architecture. Cannot continue." 15 45
 		exit 1
 		;;
 esac
@@ -71,13 +71,13 @@ if [ $arch == "i386" ];
 then
 	# i386 distro list
 	distro=$(whiptail --backtitle PRootify \
-		--menu "Select Distro" 15 40 7 \
-		alpine "Alpine" \
+		--menu "Select Distro" 15 45 7 \
+		alpine-mini "Alpine" \
 		backbox "BackBox" \
 		centos "CentOS" \
 		debian "Debian" \
 		kali "Kali" \
-		nethunter "Kali Nethunter Full" \
+		nethunter "Kali Nethunter" \
 		parrot "Parrot" \
 		ubuntu "Ubuntu" \
 		void "Void" \
@@ -86,14 +86,14 @@ then
 else
 	# arm64, armhf, and amd64 distro list
 	distro=$(whiptail --backtitle PRootify \
-		--menu "Select Distro" 15 40 7 \
-		alpine "Alpine" \
+		--menu "Select Distro" 15 45 7 \
+		alpine-mini "Alpine" \
 		backbox "BackBox" \
 		centos "CentOS" \
 		debian "Debian" \
 		fedora "Fedora" \
 		kali "Kali" \
-		nethunter "Kali Nethunter Full" \
+		nethunter "Kali Nethunter" \
 		parrot "Parrot" \
 		ubuntu "Ubuntu" \
 		void "Void" \
@@ -106,19 +106,18 @@ fi
 case $distro in
 	alpine)
 		archive_format=gz
-		dist_path="/bin:/usr/bin:/sbin:/usr/sbin"
-		dist_shell="/bin/sh --login"
+		dist_path=\\/bin:\\/usr\\/bin:\\/sbin:\\/usr\\/sbin
+		dist_shell="\\/bin\\/sh --login"
 		;;
-	backbox)
+	void)
 		archive_format=xz
-		dist_path="/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-		dist_shell="/bin/bash --login"
-	centos)
-		archive_format=xz
-		dist_path="/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin:/usr/games:/usr/local/games"
-		dist_shell="/bin/bash --login"
+		dist_path=\\/bin:\\/usr\\/bin:\\/sbin:\\/usr\\/sbin
+		dist_shell="\\/bin\\/bash --login"
+		;;
 	*)
 		archive_format=xz
+		dist_path=\\/usr\\/local\\/sbin:\\/usr\\/local\\/bin:\\/bin:\\/usr\\/bin:\\/sbin:\\/usr\\/sbin:\\/usr\\/games:\\/usr\\/local\\/games
+		dist_shell="\\/bin\\/bash --login"
 		;;
 esac
 
@@ -127,7 +126,7 @@ case $action in
 	install)
 		# Select where to install
 		dir=$(whiptail --backtitle PRootify \
-			--inputbox "Select Installation Directory (default = current user home directory)" 15 40 \
+			--inputbox "Select Installation Directory (default = current user home directory)" 15 45 \
 			"$(realpath ~)" \
 			3>&1 1>&2 2>&3)
 		exit_if_cancelled
@@ -144,7 +143,7 @@ case $action in
 		# goto folder where rootfs is
 		cd "$dir/$distro-fs"
 		echo "Decompressing RootFS..."
-		if [ "$is_termux" == "yes" ]
+		if [ "$is_termux" == "yes" ];
 		then
 			proot --link2symlink tar -xJf $dir/$distro-rootfs.tar.$archive_format
 		else
@@ -158,7 +157,89 @@ case $action in
 		# cd back to where we were before
 		cd "$currentdir"
 		mkdir -p $dir/$distro-binds
-		# todo do something
+		echo "Generating start script..."
+		# Termux start script
+		if [ "$is_termux" == yes ];
+		then
+			cat > $dir/start-$distro.sh <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+## unset LD_PRELOAD in case termux-exec is installed
+unset LD_PRELOAD
+command="proot"
+command+=" --link2symlink"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A *DIST*-binds)" ]; then
+	for f in *DIST*-binds/* ;do
+		. \$f
+	done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b *DIST*-fs/root:/dev/shm"
+## uncomment the following line to have access to the home directory of termux
+#command+=" -b /data/data/com.termux/files/home:/root"
+## uncomment the following line to mount /sdcard directly to / 
+#command+=" -b /sdcard"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=*PATH*"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" *SHELL*"
+com="\$@"
+if [ -z "\$1" ];then
+	exec \$command
+else
+	\$command -c "\$com"
+fi
+			EOM
+		else
+			cat > $dir/start-$distro.sh <<- EOM
+#!/bin/bash
+cd \$(dirname \$0)
+command="proot"
+command+=" -0"
+command+=" -r $folder"
+if [ -n "\$(ls -A *DIST*-binds)" ]; then
+	for f in *DIST*-binds/* ;do
+		. \$f
+	done
+fi
+command+=" -b /dev"
+command+=" -b /proc"
+command+=" -b *DIST*-fs/root:/dev/shm"
+## uncomment the following line to have access to the root home folder in /root
+#command+=" -b /root:/root"
+## uncomment the following line to have access to the your home folder in /root
+#command+=" -b ~:/root"
+command+=" -w /root"
+command+=" /usr/bin/env -i"
+command+=" HOME=/root"
+command+=" PATH=*PATH*"
+command+=" TERM=\$TERM"
+command+=" LANG=C.UTF-8"
+command+=" *SHELL*"
+com="\$@"
+if [ -z "\$1" ];then
+	exec \$command
+else
+	\$command -c "\$com"
+fi
+			EOM
+		fi
+		sed -i "s/\*DIST\*/$distro/g" $dir/start-$distro.sh
+		sed -i "s/\*PATH\*/$dist_path/g" $dir/start-$distro.sh
+		sed -i "s/\*SHELL\*/$dist_shell/g" $dir/start-$distro.sh
+		if [ "$is_termux" == yes ]; then termux-fix-shebang $dir/start-$distro.sh; fi
+		echo "Making $dir/start-$distro.sh executable..."
+		chmod +x $dir/start-$distro.sh
+		echo "Removing $dir/$distro-rootfs.tar.$archive_format"
+		rm $dir/$distro-rootfs.tar.$archive_format
+		echo "Done!"
+		echo "Run $dir/start-$distro.sh to drop into your new distro!"
 		;;
 	*)
 		whiptail --backtitle PRootify --msgbox "This feature hasn't been implemented yet." 15 40
